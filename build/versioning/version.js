d9c3d5c486a7583388625bb0f8ca94ea3b07fe98
Romain Lebossé <romain.lebosse@klaxoon.com>
2020-12-01 11:42:57 +0100

    Push build folder

---
+++ build/versioning/version.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    "dirty": false,
+    "raw": "v2.1.0-10-g4cfcf04",
+    "hash": "g4cfcf04",
+    "distance": 10,
+    "tag": "v2.1.0",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v2.1.0",
+        "major": 2,
+        "minor": 1,
+        "patch": 0,
+        "prerelease": [],
+        "build": [],
+        "version": "2.1.0"
+    },
+    "suffix": "10-g4cfcf04",
+    "semverString": "2.1.0+10.g4cfcf04"
+};
+//# sourceMappingURL=version.js.map
